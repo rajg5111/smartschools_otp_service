f02700d16971af36ee290f130202fb05c6810d76
VEERA GUNDU <veera.g5111@gmail.com>
2025-11-01 11:38:54 +1100

    auth dev

--- lib/smartschools_otp_service-stack.ts
+++ lib/smartschools_otp_service-stack.ts
@@ -231,7 +231,7 @@ export class SmartSchoolsOtpServiceStack extends cdk.Stack {
     // Create a Route 53 A record
     new route53.ARecord(this, "ApiARecord", {
       zone: hostedZone,
-      recordName: "auth",
+      recordName: `${props.environment}.auth`,
       target: route53.RecordTarget.fromAlias(
         new route53Targets.ApiGatewayDomain(customDomain)
       ),
